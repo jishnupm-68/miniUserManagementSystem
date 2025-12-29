84f93d53a5107c41f20730f2775e203ca247435f
Jishnu PM <jishnupmbcr68@gail.com>
2025-12-29 20:10:19 +0530

    backend setup : deleted unwanted file

--- backend/helper/passwordStrengthChecker.js
+++
@@ -1,6 +0,0 @@
-const  isStrongPassword = (password)=> {
-  const strongPasswordRegex =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-  return strongPasswordRegex.test(password);
-}
-module.exports = isStrongPassword
