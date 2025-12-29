f336b0d4161e89cec4b17ecb78f8b04ce2cb1fab
Jishnu PM <jishnupmbcr68@gail.com>
2025-12-29 14:34:53 +0530

    backend setup : userSchema

---
+++ backend/model/userSchema.js
@@ -0,0 +1,57 @@
+const mongoose =require("mongoose");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        validate(value){
+            if(!validator.isEmail(value)) throw new Error("Please provide a email")
+        }
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    fullName: {
+        type:String,
+        minLength:[3, "full name must have atleast 3 characters"],
+        maxLength:[120, "full name must have atmost 120 characters"],
+        required:true
+    },
+    role:{
+        type:String,
+        enum:["admin", "user"],
+        required:true
+    },
+    status:{
+        type:String,
+        enum:["active", "inactive"],
+        default:"active",
+    },
+    lastLogin:{
+        type:Date,
+        default:null
+    }
+})
+
+userSchema.methods.getJwt = async function(){
+    let user = this;
+    const token = await jwt.sign(
+        {_id:user?._id},
+        process.env.JWT_SECRET,
+        {expiresIn:"12h"}
+    )
+    return token;
+}
+
+userSchema.methods.comparePassword = async function(inputPassword){
+    let user =this;
+    let hash = user.password;
+    const result = await bcrypt.compare(inputPassword, hash);
+    return result
+}
+
+const User = mongoose.model("User", userSchema);
